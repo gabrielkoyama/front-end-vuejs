6b435deb19d9eea2cf6d9ac0b9ec1358d24d544d
lukasribeiro <luckas.ribeiro2015@gmail.com>
2019-05-24 10:29:30 -0300

    telinha de reserva

--- src/views/Reserva.vue
+++ src/views/Reserva.vue
@@ -1,47 +1,68 @@
 <template>
+  <!-- reserva -->
+  <div class="car1d">
+    <div class="card-body p-0">
+      <h5 class="card-title" style="margin: 10px 10px 0px 10px; padding: 0.65rem; "> </h5>
 
-<div class="container mt-5">
-  <fieldset>
-    <legend class=''><strong>Lista de Carros Disponiveis</strong></legend>
-    <p>
-      Aqui voce podera consultar os carros que ja estão reservados e os carros que estao disponveis
+        <div class="container">
+          
+        <h3>Reserva atual</h3>
+        <p>Sua reserva atual é mostrada abaixo</p>
 
+          <table class="table table-stripped mt-2 carros">
+            <thead>
+              <tr>
+                <th>Nome do carro</th>
+                <th>Modelo</th>
+                <th>Data Inicio</th>
+                <th>Data Fim</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="res in reserva_at" :key="res.id">
+                <td>{{}}</td>
+                <td>{{}}</td>
+                <td>{{}}</td>
+                <td>{{}}</td>
+                <!-- <td> <a style="cursor: pointer"> <i class="fas fa-edit fa-sm"></i> </a> </td> -->
+                <td> <a style="cursor: pointer" :id="car.id" @click="deleteCarById"> <i class="fas fa-trash fa-sm"
+                      style="color: red"></i> </a> </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
 
-    </p>
-  <table class="table">
-    <thead class="thead-dark">
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Nome</th>
-        <th scope="col">Km</th>
-        <th scope="col">Categoria</th>
-        <th scope="col">Modelo</th>
-        <th scope="col">Ano</th>
-        <th scope="col">Fabricante</th>
-        <th scope="col">Cor</th>
-        <th scope="col">Disponivel</th>
-        <th scope="col">Status</th>
+        <div class="dropdown-divider mt-5"></div>
+      
+      <div class="container">
+        <div class="mt-5">
+          
+          <h3>Historic  o de reservas</h3>
+          <p>Seu historico de reservas e mostrado abaixo</p>
 
-      </tr>
-    </thead>
-    <tbody>
-       <tr>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-      </tr>
-    </tbody>
-  </table>
-    
-  <button type="button" class="btn btn-success">Meus carros reservados</button>
-  </fieldset>
-</div>
+          <table class="table table-stripped mt-2 carros">
+            <thead>
+              <tr>
+                <th>Nome do carro</th>
+                <th>Modelo</th>
+                <th>Data Inicio</th>
+                <th>Data Fim</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="res in reserva" :key="res.id">
+                <td>{{}}</td>
+                <td>{{}}</td>
+                <td>{{}}</td>
+                <td>{{}}</td>
+                <td> <a style="cursor: pointer" :id="car.id" @click="deleteCarById"> <i class="fas fa-trash fa-sm" style="color: red"></i> </a> </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -51,10 +72,13 @@
   } from 'vuex'
 
   export default {
+
     name: 'reserva',
+
     data() {
       return {
-        carros: []
+        reserva: [],
+        reserva_at:[]
       }
     }
   }
